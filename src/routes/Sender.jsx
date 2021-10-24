55328c121cea83248286e98712bda23fdb745e65
mattrossman <rossman@gatech.edu>
2021-10-24 16:14:01 -0400

    Add DOM overlay to sender

--- src/routes/Sender.jsx
+++ src/routes/Sender.jsx
@@ -1,14 +1,22 @@
 import { ARCanvas } from '@react-three/xr'
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
+import { createPortal } from 'react-dom'
 
 import { Fullscreen } from '@/components/dom/Fullscreen'
 import { AttachToCamera } from '@/components/three/AttachToCamera'
 import Avatar from '@/components/three/Avatar'
 
 export function Sender() {
+  const { rootEl, DomOverlay } = useDomOverlay()
   return (
     <Fullscreen>
-      <ARCanvas sessionInit={{ requiredFeatures: ['worldSensing'] }} camera={{ fov: 35 }}>
+      <ARCanvas
+        sessionInit={{
+          requiredFeatures: ['worldSensing', 'dom-overlay'],
+          domOverlay: { root: rootEl },
+        }}
+        camera={{ fov: 35 }}
+      >
         <Suspense fallback={null}>
           <AttachToCamera>
             <group position={[0, -0.6, -1]}>
@@ -19,6 +27,23 @@ export function Sender() {
           </AttachToCamera>
         </Suspense>
       </ARCanvas>
+      <DomOverlay>
+        <div style={{ position: 'absolute', inset: 0 }}>
+          <div>Hello Overlay!</div>
+        </div>
+      </DomOverlay>
     </Fullscreen>
   )
+}
+
+function useDomOverlay() {
+  const [rootEl] = useState(() => {
+    const overlayElement = document.createElement('div')
+    document.body.appendChild(overlayElement)
+    return overlayElement
+  })
+  function DomOverlay({ children }) {
+    return createPortal(children, rootEl)
+  }
+  return { rootEl, DomOverlay }
 }

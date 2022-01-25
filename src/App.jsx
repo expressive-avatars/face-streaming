9c0e04b02c3a9ada97ac04494f2f54a13014d7c9
mattrossman <rossman@gatech.edu>
2022-01-24 22:21:11 -0500

    Check WebXRViewer in userAgent

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,39 @@ export default function App() {
 
 function LandingPage({ onStartAR }) {
   const snap = useStore()
+  const isSupportedBrowser = navigator.userAgent.includes('WebXRViewer')
+  let body
+  if (isSupportedBrowser) {
+    if (snap.credentials) {
+      body = <SignedIn onStartAR={onStartAR} />
+    } else {
+      body = <SignedOut />
+    }
+  } else {
+    body = <UnsupportedBrowser />
+  }
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
       <img alt="Augmented Environments Lab logo" src="/ael-logo.jpg" width={200} />
-      {snap.credentials ? <SignedIn onStartAR={onStartAR} /> : <SignedOut />}
+      {body}
+    </div>
+  )
+}
+
+function UnsupportedBrowser() {
+  return (
+    <div className="grid gap-10 place-items-center px-8">
+      <p className="text-center">Please visit this page in WebXR Viewer on a supported iOS device.</p>
+      <a
+        href="https://apps.apple.com/us/app/webxr-viewer/id1295998056?itsct=apps_box_badge&amp;itscg=30200"
+        style={{ display: 'inline-block', overflow: 'hidden', borderRadius: '13px', width: '250px', height: '83px' }}
+      >
+        <img
+          src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1512604800&h=337a4ee86d323f6cc8ce8ab793b4b045"
+          alt="Download on the App Store"
+          style={{ borderRadius: '13px', width: '250px', height: '83px' }}
+        />
+      </a>
     </div>
   )
 }
